87bf44c4a21bae6a1130ef62f4835beb5eae9ab1
iLia Railian <raylyanway@gmail.com>
2022-12-04 15:56:24 -0800

    feat(*): update logger

--- startup/logging.js
+++ startup/logging.js
@@ -1,14 +1,16 @@
 import { createLogger, transports, format } from "winston";
-// require('winston-mongodb');
-import("express-async-errors");
+import "winston-mongodb";
+import "express-async-errors";
 
 export const logger = createLogger({
-  transports: [new transports.File({ filename: "combined.log" })],
+  transports: [
+    new transports.File({ filename: "combined.log" }),
+    new transports.MongoDB({ db: "mongodb://localhost/vidly", level: "info" }),
+  ],
   exceptionHandlers: [
     new transports.Console({
       format: format.combine(format.colorize(), format.prettyPrint()),
     }),
-    // new transports.Console({ colorize: true, prettyPrint: true }),
     new transports.File({ filename: "uncaughtExceptions.log" }),
   ],
 });
@@ -16,20 +18,3 @@ export const logger = createLogger({
 process.on("unhandledRejection", (ex) => {
   throw ex;
 });
-
-// module.exports.run = function () {
-// winston.exceptions.handle(
-//   new winston.transports.Console({ colorize: true, prettyPrint: true }),
-//   new winston.transports.File({ filename: "uncaughtExceptions.log" })
-// );
-
-// process.on("unhandledRejection", (ex) => {
-//   throw ex;
-// });
-
-// winston.add(winston.transports.File, { filename: "logfile.log" });
-// winston.add(winston.transports.MongoDB, {
-//   db: 'mongodb://localhost/vidly',
-//   level: 'info'
-// });
-// };
